0e951275a0bd0984920b68a7ba776dbda9337f04
Yusuf Sabbag <sist.heb@gmail.com>
2021-11-07 20:40:05 +0300

    updated port variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const { apiRouter } = require("./router/api/comments");
 const { router } = require("./router/user/comments");
